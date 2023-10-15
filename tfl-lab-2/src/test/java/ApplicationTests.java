9a9e861e341a5caac4ad72d712c87197e5c6c923
a.shevyrov <108514147+ArkadiyShevyrov@users.noreply.github.com>
2023-10-15 22:28:56 +0300

    TFL-2. Add lab 1

--- tfl-lab-2/src/test/java/ApplicationTests.java
+++ tfl-lab-2/src/test/java/ApplicationTests.java
@@ -1,15 +1,7 @@
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ContextConfiguration;
-import ru.bmstu.iu9.tfl_lab_2.controller.CController;
 import ru.bmstu.iu9.tfl_lab_2.service.OptimizationService;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,8 +21,6 @@ class ApplicationTests {
         list.add(new TestA("(((a|c|(a|b)c|c|(a|b)c|c|a(b|c)|a|b|a)|b)*|((acd)e)*)*", "(a|ab|ac|ac|acde|b|bc|c)*"));
         list.add(new TestA("(a|b)c", "ac|bc"));
         list.add(new TestA("a(b|c)", "ab|ac"));
-//        list.add(new TestA("", ""));
-//        list.add(new TestA("", ""));
         for (TestA testA : list) {
             String optimization = optimizationService.optimization(testA.input);
             assertEquals(optimization, testA.expected);

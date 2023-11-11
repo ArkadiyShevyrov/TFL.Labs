e93889bb8275055abf3600da2878b328728bb9b1
a.shevyrov <108514147+ArkadiyShevyrov@users.noreply.github.com>
2023-11-11 19:01:00 +0300

    TFL-2. Add lab 1

--- tfl-lab-lib/src/main/java/ru/bmstu/iu9/tfl_lab_lib/model/DFA.java
+++ tfl-lab-lib/src/main/java/ru/bmstu/iu9/tfl_lab_lib/model/DFA.java
@@ -1,9 +1,10 @@
 package ru.bmstu.iu9.tfl_lab_lib.model;
 
 import lombok.AllArgsConstructor;
-import java.util.List;
+import lombok.Getter;
 import java.util.Set;
 
+@Getter
 @AllArgsConstructor
 public class DFA {
     private Set<State> states;
@@ -11,21 +12,4 @@ public class DFA {
     private State initialState;
     private Set<State> finalStates;
     private TransitionFunctionDFA transitionFunction;
-
-    public boolean isValidString(List<Symbol> symbols) {
-        State currentState = this.initialState;
-        for (Symbol symbol : symbols) {
-            State transitionState = this.transitionFunction.transition(currentState, symbol);
-            if (transitionState != null) {
-                currentState = transitionState;
-            } else {
-                return false;
-            }
-        }
-        return containsState(currentState, this.finalStates);
-    }
-
-    private boolean containsState(State state, Set<State> states) {
-        return states.stream().anyMatch(s -> s.equals(state));
-    }
 }
---
+++ tfl-lab-lib/src/main/java/ru/bmstu/iu9/tfl_lab_lib/model/Language.java
@@ -0,0 +1,25 @@
+package ru.bmstu.iu9.tfl_lab_lib.model;
+
+import java.util.List;
+import java.util.Set;
+
+public class Language {
+//    private FA automate;
+//
+//        public boolean isValidString(List<Symbol> symbols) {
+//        State currentState = this.initialState;
+//        for (Symbol symbol : symbols) {
+//            State transitionState = this.transitionFunction.transition(currentState, symbol);
+//            if (transitionState != null) {
+//                currentState = transitionState;
+//            } else {
+//                return false;
+//            }
+//        }
+//        return containsState(currentState, this.finalStates);
+//    }
+//
+//    private boolean containsState(State state, Set<State> states) {
+//        return states.stream().anyMatch(s -> s.equals(state));
+//    }
+}
